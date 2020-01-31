95b966ee832edf572f18d132397feb8880dbd2b4
nevty <theacutemace@gmail.com>
2020-01-31 13:45:15 +0700

    Updates

---
+++ precache-manifest.730276545262adafaa97cece4afd0e90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2776de6eaf084a0dcd3ff6942d600a48",
+    "url": "/test-avia/index.html"
+  },
+  {
+    "revision": "c762bea3d01c4ef63bc2",
+    "url": "/test-avia/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "995bea96be9e236c3888",
+    "url": "/test-avia/static/css/main.dd0f99f0.chunk.css"
+  },
+  {
+    "revision": "c762bea3d01c4ef63bc2",
+    "url": "/test-avia/static/js/2.a6039108.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/test-avia/static/js/2.a6039108.chunk.js.LICENSE"
+  },
+  {
+    "revision": "995bea96be9e236c3888",
+    "url": "/test-avia/static/js/main.9281cc50.chunk.js"
+  },
+  {
+    "revision": "f085b9d92d17ce3b00b3",
+    "url": "/test-avia/static/js/runtime-main.d8661ec1.js"
+  },
+  {
+    "revision": "bc5f6531f6f37e071c68939b6b851627",
+    "url": "/test-avia/static/media/error_face.bc5f6531.svg"
+  },
+  {
+    "revision": "dd6c5e8f783c67f34d270b510d1e4af2",
+    "url": "/test-avia/static/media/loading.dd6c5e8f.svg"
+  }
+]);
